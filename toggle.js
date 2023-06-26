fa14626fc2d9474791e54a948958824a5de484ed
Suzie-to <yahiya.zyahiya@gmail.com>
2023-06-26 14:04:18 +0100

    included second toggle, addedd 'mailto' to contact form

---
+++ toggle.js
@@ -0,0 +1,8 @@
+function myToggle() {
+    var toggle = document.getElementById("myToggle");
+    if (toggle.style.display === "none") {
+      toggle.style.display = "block";
+    } else {
+      toggle.style.display = "none";
+    }
+  }
