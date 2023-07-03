54a2facc2a187bbca956a12d977a471ef0fbc58f
Suzie-to <yahiya.zyahiya@gmail.com>
2023-07-03 09:01:25 +0100

    added more images and cleaned up the css

--- toggle.js
+++ toggle.js
@@ -6,3 +6,11 @@ function myToggle() {
       toggle.style.display = "none";
     }
   }
+  function myToggle1() {
+    var toggle = document.getElementById("myToggle1");
+    if (toggle.style.display === "none") {
+      toggle.style.display = "block";
+    } else {
+      toggle.style.display = "none";
+    }
+  }
