73fdf6382636bd1b5be9c0dcb6d51767377a94d3
Suzie-to <yahiya.zyahiya@gmail.com>
2023-07-03 17:34:31 +0100

    fixed toggle double click, eduted text and layout

--- toggle.js
+++ toggle.js
@@ -1,13 +1,46 @@
 function myToggle() {
-    var toggle = document.getElementById("myToggle");
+  var toggle = document.getElementById("aboutMeToggle");
+  if (toggle.style.display === "none") {
+    toggle.style.display = "block";
+  } else {
+    toggle.style.display = "none";
+  }
+}
+
+// function myToggle() {
+//     var toggle = document.getElementById("myToggle");
+//     if (toggle.style.display === "none") {
+//       toggle.style.display = "block";
+//     } else {
+//       toggle.style.display = "none";
+//     }
+//   }
+  function myToggle1() {
+    var toggle = document.getElementById("workToggle");
     if (toggle.style.display === "none") {
       toggle.style.display = "block";
     } else {
       toggle.style.display = "none";
     }
   }
-  function myToggle1() {
-    var toggle = document.getElementById("myToggle1");
+  function myToggle2() {
+    var toggle = document.getElementById("educationToggle");
+    if (toggle.style.display === "none") {
+      toggle.style.display = "block";
+    } else {
+      toggle.style.display = "none";
+    }
+  }
+  function myToggle3() {
+    var toggle = document.getElementById("whyToggle");
+    if (toggle.style.display === "none") {
+      toggle.style.display = "block";
+    } else {
+      toggle.style.display = "none";
+    }
+  }
+  function myToggle4() {
+    var toggle = document.getElementById("kukeriToggle");
     if (toggle.style.display === "none") {
       toggle.style.display = "block";
     } else {
